ac7f6f769ff51f3dbfd475f42da98792153940f5
david-cc <david.caudeli@gmail.com>
2016-10-06 15:10:57 +0200

    Finition CSS et jQuery du div bigCallout

--- includes/js/script.js
+++ includes/js/script.js
@@ -8,3 +8,15 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+$(function(){
+  $('#alertMe').click(function(e){
+    e.preventDefault();
+    $('#successAlert').slideDown();
+  });
+
+  $('#alertHide').click(function(e){
+    e.preventDefault();
+    console.log("button close clicked");
+    $('#successAlert').slideUp();
+  });
+});
